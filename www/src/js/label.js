4bce709272b8dd62f262fcd60449f7372067ccf2
dominique-roux <dominique.roux@me.com>
2017-10-03 18:19:29 +0200

    Major refactor

--- www/src/js/label.js
+++ www/src/js/label.js
@@ -1,198 +1,233 @@
+const r = 600/25.4; //ratio: 600 dpi / 25.4mm per inch
+const rs = 72/25.4 *2; //ratio on the screen: 72 dpi / 24.4mm per inch ()*2 because it looks nicer...)
 
+var printService;
+var labels;
 
-			const r = 310; //ratio to apply to get the size with printer unit. Empiric determination...
+$(function(){
+	loadConfig()
+});
 
-			$(function(){
-				//Each label
-				for (var i = 0; i < config.length; i++) {
-					initLabel(i, config[i])
-					var labelConfig = config[i];
-					//each element within the label
-					for (var j = 0; j < labelConfig.elements.length; j++) {
-						var element = labelConfig.elements[j];
-						var paramName = element.content
-						var value  = getParameterByName(paramName)
-						if ( element.type =="text") {
-							addText(i, value, element.x, element.y, element.w, element.fontSize)
-						}
-						if ( element.type =="qrcode") {
-							addQrCode(i, value, element.x, element.y, element.h, element.w)
-						}
-					}
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+function loadConfig() {
+	var configFile = getUrlParameter("config")
+	var jqxhr = $.getJSON( "conf/"+configFile, function(config) {
+		printService = config.printService;
+		labels = config.labels;
+
+		//Each label
+		for (var i = 0; i < labels.length; i++) {
+			initLabel(i, labels[i])
+			var labelConfig = labels[i];
+			//each element within the label
+			drawBorder(i);
+			for (var j = 0; j < labelConfig.elements.length; j++) {
+				var element = labelConfig.elements[j];
+				var paramName = element.content
+				var value  = getParameterByName(paramName)
+				if ( element.type =="text") {
+					addText(i, value, element.x, element.y, element.w, element.fontSize)
 				}
-		});
-
-		function getParameterByName(name, url) {
-		    if (!url) {
-		      url = window.location.href;
-		    }
-		    name = name.replace(/[\[\]]/g, "\\$&");
-		    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		        results = regex.exec(url);
-		    if (!results) return null;
-		    if (!results[2]) return '';
-		    return decodeURIComponent(results[2].replace(/\+/g, " "));
+				if ( element.type =="qrcode") {
+					addQrCode(i, value, element.x, element.y, element.h, element.w)
+				}
+			}
 		}
+  })
+  .fail(function( jqxhr, textStatus, error ) {
+    var err = textStatus + ", " + error;
+    console.log( "Request Failed: " + err );
+  })
+}
 
-		function splitText(ctx,text,maxPxLength) {
-			var textLines = text.split("\n");
-			var data = [];
-			for (var j = 0; j < textLines.length; j++) {
-				var wa=textLines[j].split(" "),
-		        lastPhrase=wa[0],
-		        measure=0,
-		        splitChar=" ";
-		    if (wa.length <= 1) {
+function getParameterByName(name, url) {
+	if (!url) {
+		url = window.location.href;
+	}
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function splitText(ctx,text,maxPxLength) {
+	var textLines = text.split("\n");
+	var data = [];
+	for (var j = 0; j < textLines.length; j++) {
+		var wa=textLines[j].split(" "),
+        lastPhrase=wa[0],
+        measure=0,
+        splitChar=" ";
+    if (wa.length <= 1) {
+			data.push(lastPhrase);
+    }
+    for (var i=1;i<wa.length;i++) {
+        var w=wa[i];
+        measure=ctx.measureText(lastPhrase+splitChar+w).width;
+        if (measure<maxPxLength) {
+            lastPhrase+=(splitChar+w);
+        } else {
 					data.push(lastPhrase);
-		    }
-		    for (var i=1;i<wa.length;i++) {
-		        var w=wa[i];
-		        measure=ctx.measureText(lastPhrase+splitChar+w).width;
-		        if (measure<maxPxLength) {
-		            lastPhrase+=(splitChar+w);
-		        } else {
-							data.push(lastPhrase);
 
-		            lastPhrase=w;
-		        }
-		        if (i===wa.length-1) {
-								data.push(lastPhrase);
-		            break;
-		        }
-		    }
-			}
-		  return data
-		}
+            lastPhrase=w;
+        }
+        if (i===wa.length-1) {
+						data.push(lastPhrase);
+            break;
+        }
+    }
+	}
+  return data
+}
 
-		function getQRCodeImage(id ,text) {
-			//draw QR Code
-			qrId = "qr_"+id
-			jQuery('#'+qrId).qrcode({
-				text	: text,
-				width : 256,
-				height: 256,
-				type: "qrcode",
-				correctLevel: QRErrorCorrectLevel.H
-			});
-			var barcodeCanvas = document.getElementById(qrId).childNodes[0];
-			var barcode = barcodeCanvas.toDataURL();
-			document.getElementById(qrId).style.display = 'none'; // Hide
-			return barcode;
+function getQRCodeImage(id ,text) {
+	//draw QR Code
+	qrId = "qr_"+id
+	jQuery('#'+qrId).qrcode({
+		text	: text,
+		width : 256,
+		height: 256,
+		type: "qrcode",
+		correctLevel: QRErrorCorrectLevel.H
+	});
+	var barcodeCanvas = document.getElementById(qrId).childNodes[0];
+	var barcode = barcodeCanvas.toDataURL();
+	document.getElementById(qrId).style.display = 'none'; // Hide
+	return barcode;
+}
 
-		}
+function initLabel(id, config) {
+	//create row
+	var rowId = "row"+id
+	var row = document.createElement('div');
+	row.id = rowId;
+	row.className = "row margin well";
+	document.getElementById("main").appendChild(row);
 
-		function initLabel(id, config) {
+	//create Desc
+	var descColId = "descCol_"+rowId
+	var descCol = document.createElement('div');
+	descCol.id = descColId;
+	descCol.className = 'col-sm-3 vcenter';
+	descCol.innerHTML = "<blockquote>"+config.name+"</blockquote>";
+	document.getElementById(rowId).appendChild(descCol);
 
-			//create row
-			var rowId = "row"+id
-			var row = document.createElement('div');
-			row.id = rowId;
-			row.className = "row margin well";
-			document.getElementById("main").appendChild(row);
+	//create col2
+	var col1Id = "col1_"+rowId
+	var col1 = document.createElement('div');
+	col1.id = col1Id;
+	col1.className = 'col-sm-7';
+	document.getElementById(rowId).appendChild(col1);
 
-			//create Desc
-			var descColId = "descCol_"+rowId
-			var descCol = document.createElement('div');
-			descCol.id = descColId;
-			descCol.className = 'col-sm-3 vcenter';
-			descCol.innerHTML = "<blockquote>"+config.name+"</blockquote>";
-			document.getElementById(rowId).appendChild(descCol);
+	//create label
+	var labelId = "label_"+id
+	var label = document.createElement('div');
+	label.id = labelId;
+	label.className = 'img-thumbnail';
+	document.getElementById(col1Id).appendChild(label);
 
-			//create col2
-			var col1Id = "col1_"+rowId
-			var col1 = document.createElement('div');
-			col1.id = col1Id;
-			col1.className = 'col-sm-7';
-			document.getElementById(rowId).appendChild(col1);
+	//create canvas
+	var canvasId = "canvas_"+labelId
+	var canvas = document.createElement('canvas');
+	canvas.id = canvasId;
+	canvas.width = config.width * r;
+	canvas.height = config.height * r;
 
-			//create label
-			var labelId = "label_"+id
-			var label = document.createElement('div');
-			label.id = labelId;
-			label.className = 'img-thumbnail';
-			document.getElementById(col1Id).appendChild(label);
+	canvas.style.width = config.width * rs + 'px';
+	canvas.style.height = config.height * rs + 'px';
 
-			//create canvas
-			var canvasId = "canvas_"+labelId
-			var canvas = document.createElement('canvas');
-			canvas.id = canvasId;
-			canvas.width = config.width * 10;
-			canvas.height = config.height * 10;
+	canvas.style.backgroundColor = "white"
+	document.getElementById(labelId).appendChild(canvas);
 
-			canvas.style.width = config.width*1.5 + 'px';
-			canvas.style.height = config.height*1.5 + 'px';
+	//create col2
+	var col2Id = "col2_"+rowId
+	var col2 = document.createElement('div');
+	col2.id = col2Id;
+	col2.className = 'col-sm-2 vcenter';
+	document.getElementById(rowId).appendChild(col2);
 
-			canvas.style.backgroundColor = "white"
-			document.getElementById(labelId).appendChild(canvas);
+	//Print Button
+	var printId = "print_"+col2Id
+	var print = document.createElement('button');
+	print.id = printId;
+	print.className = 'btn btn-primary btn-lg';
+	print.innerHTML = 'Print';
+	print.onclick = function(){
+		printLabel(canvasId, config);
+	};
+	document.getElementById(col2Id).appendChild(print);
 
-			//create col2
-			var col2Id = "col2_"+rowId
-			var col2 = document.createElement('div');
-			col2.id = col2Id;
-			col2.className = 'col-sm-2 vcenter';
-			document.getElementById(rowId).appendChild(col2);
+	var qrId = "qr_"+id
+	var qr = document.createElement('div');
+	qr.id = qrId;
+	document.getElementById("main").appendChild(qr);
+}
 
-			//Print Button
-			var printId = "print_"+col2Id
-			var print = document.createElement('button');
-			print.id = printId;
-			print.className = 'btn btn-primary btn-lg';
-			print.innerHTML = 'Print';
-			print.onclick = function(){
-    		printLabel(canvasId, config);
-  		};
-			document.getElementById(col2Id).appendChild(print);
+function addQrCode(id, text, x, y, h, w) {
+	divId = "label_"+id
+	canvasId = "canvas_"+divId
 
-			var qrId = "qr_"+id
-			var qr = document.createElement('div');
-			qr.id = qrId;
-			document.getElementById("main").appendChild(qr);
-		}
+	var cnvs = document.getElementById(canvasId);
+	var ctx = cnvs.getContext("2d");
 
+	var qrImage = new Image();
+	qrImage.src = getQRCodeImage(id, text);
 
-		function addQrCode(id, text, x, y, h, w) {
-			divId = "label_"+id
-			canvasId = "canvas_"+divId
+	qrImage.onload = function() {
+		ctx.drawImage(qrImage, x*r, y*r, h*r,w*r);
+	};
+}
 
-			var cnvs = document.getElementById(canvasId);
-			var ctx = cnvs.getContext("2d");
+function drawBorder(id) {
+	if (labels[id].border != null && labels[id].border)
+	{
+		divId = "label_"+id
+		canvasId = "canvas_"+divId
 
-			var qrImage = new Image();
-			qrImage.src = getQRCodeImage(id, text);
+		var c = document.getElementById(canvasId);
+		var ctx = c.getContext("2d");
+		ctx.lineWidth=3;
+		ctx.rect(3, 3, c.width-6, c.height-6);
+		ctx.stroke();
+	}
+}
 
-			qrImage.onload = function() {
-				ctx.drawImage(qrImage, x*r, y*r, h*r,w*r);
-			};
-		}
+function addText(id, text, x, y, w, fontsize) {
+	divId = "label_"+id
+	canvasId = "canvas_"+divId
 
-		function addText(id, text, x, y, w, fontsize) {
-			divId = "label_"+id
-			canvasId = "canvas_"+divId
+	var c = document.getElementById(canvasId);
+	var ctx = c.getContext("2d");
+	ctx.font = (fontsize*r)+"px Arial"
+	textLines = splitText(ctx,text,w*r)
 
-			var c = document.getElementById(canvasId);
-			var ctx = c.getContext("2d");
-			ctx.font = (fontsize*r)+"px Arial"
-			textLines = splitText(ctx,text,w*r)
+	for (var i = 0; i < textLines.length; i++) {
+		ctx.fillText(textLines[i], x*r, (y + (fontsize*1.1)*(i+1))*r);
+	}
+}
 
-			for (var i = 0; i < textLines.length; i++) {
-				ctx.fillText(textLines[i], x*r, (y + (fontsize*1.1)*(i+1))*r);
-			}
-		}
+function printLabel(canvasId, config) {
+	var c = document.getElementById(canvasId);
+	var ctx = c.getContext("2d");
+	//Rotate 90° because the printer needs vertical orientation
+	ctx.rotate(-Math.PI/2);
+	var image = c.toDataURL();
+	var url = printService+config.label;
+	console.log(url);
 
-		function printLabel(canvasId, config) {
-			var c = document.getElementById(canvasId);
-			var ctx = c.getContext("2d");
-			//Rotate 90° because the printer needs vertical orientation
-			ctx.rotate(Math.PI/2);
-			var image = c.toDataURL();
-			var url = printService+"w"+config.height+"h"+config.width;
-			console.log(url);
-
-			$.ajax({
-		    type: "POST",
-		    url: url,
-		    data: {
-		        base64data : image
-		    }
-			});
-		}
+	$.ajax({
+    type: "POST",
+    url: url,
+    data: {
+        base64data : image
+    }
+	});
+}
